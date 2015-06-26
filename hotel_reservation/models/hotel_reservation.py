1d05dc69d83a50f3196c1b72a8852e90942d99e8
Malay Khamar <m.khamar.serpentcs@gmail.com>
2015-06-26 18:58:23 +0530

    [MODIFY]:modify all index.html in vertical_hotel module

--- hotel_reservation/models/hotel_reservation.py
+++ hotel_reservation/models/hotel_reservation.py
@@ -173,41 +173,6 @@ class hotel_reservation(models.Model):
             myobj.send_mail(user.id, force_send=True, raise_exception=True)
         return True 
 
-    @api.multi
-    def action_for_hotel(self):
-        '''
-        This function opens a window to compose an email, with the edi sale template message loaded by default
-        '''
-        assert len(self._ids) == 1, 'This option should only be used for a single id at a time.'
-        ir_model_data = self.env['ir.model.data']
-        try:
-            template_id = ir_model_data.get_object_reference('hotel_reservation', 'email_template_hotel_reservation')[1]
-        except ValueError:
-            template_id = False
-        try:
-            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
-        except ValueError:
-            compose_form_id = False 
-        ctx = dict()
-        ctx.update({
-            'default_model': 'hotel.reservation',
-            'default_res_id': self._ids[0],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
-            'default_composition_mode': 'comment',
-            'mark_so_as_sent': True
-        })
-        return {
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'mail.compose.message',
-            'views': [(compose_form_id, 'form')],
-            'view_id': compose_form_id,
-            'target': 'new',
-            'context': ctx,
-        }
-
 
     @api.multi
     def _create_folio(self):
